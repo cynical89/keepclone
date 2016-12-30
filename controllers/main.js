16030bf7d9134e97717f97cb3d018fba23151b96
Anthony Michelizzi <cynical89@gmail.com>
2016-12-29 18:09:56 -0800

    Cleaned up code. Have hbs pages rendering

---
+++ controllers/main.js
@@ -0,0 +1,4 @@
+
+module.exports.index = function index(req, res) {
+  res.render("index");
+};
--- server.js
+++ server.js
@@ -1,56 +1,57 @@
-var express = require('express');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var mongo = require('mongodb');
-var cookieParser = require('cookie-parser');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const mongo = require("mongodb");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const morgan = require("morgan");
+const passport = require("passport");
+const expressValidator = require("express-validator");
 
 
-var routes = require('./routes/index');
-var admin = require('./routes/admin');
-var api = require('./routes/api');
+const main = require("./controllers/main");
 
-var app = express();
+const app = express();
 
+const config = require("./config.json");
+const viewdir = __dirname + "/views/";
+const basedir = __dirname + "/public/";
 
-const config = require('./config.json');
-const viewdir = __dirname + '/views/';
-const basedir = __dirname + '/public/';
-
-app.set('views', viewdir);
-app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
-app.set('view engine', 'handlebars');
+app.set("views", viewdir);
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 app.use(bodyParser.json()); // Support JSON Encoded bodies
-app.use(bodyParser.urlencoded({extended: true})); // Support encoded bodies 
+app.use(bodyParser.urlencoded({extended: true})); // Support encoded bodies
 app.use(cookieParser()); // Use cookieparser
 app.use(express.static(basedir));
 
 // Express Session
 app.use(session({
-  secret: 'keepclone',
+  secret: config.site.secret,
   saveUninitialized: true,
-  resave: true
+  resave: true,
+  cookie: { secure: true }
 }));
 
-if(process.env.NODE_ENV = 'development') {
-  app.use(morgan('dev'));
+if(process.env.NODE_ENV = "development") {
+  app.use(morgan("dev"));
 }
 
 // Passport init
-require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Express validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
+      const namespace = param.split(".")
       , root    = namespace.shift()
       , formParam = root;
 
     while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      formParam += "[" + namespace.shift() + "]";
     }
     return {
       param : formParam,
@@ -62,24 +63,19 @@ app.use(expressValidator({
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 // Make our db accessible to our router
-app.use(function(req, res, next) {
-  req.db = db;
-  next();
+// app.use(function(req, res, next) {
+//   req.db = db;
+//   next();
+// });
+
+app.use("/", main.index);
+
+app.set("port", (process.env.PORT || config.site.port));
+
+app.listen(app.get("port"), function() {
+  console.log(config.site.name + " running on port " + app.get("port"));
 });
-
-app.use('/', routes);
-app.use('/admin', admin);
-app.use('/api', api);
-
-app.set('port', (process.env.PORT || config.site.port));
-
-app.listen(app.get('port'), function() {
-  console.log(config.site.name + " running on port " + app.get('port'));
-});
-
-
-
