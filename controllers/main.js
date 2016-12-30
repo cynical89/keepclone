dff330d0194044736a564fbc4036eaa9f1d6c83c
Anthony Michelizzi <cynical89@gmail.com>
2016-12-29 18:17:58 -0800

    Getting title from config

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,7 @@
+const config = require("../config.json");
 
 module.exports.index = function index(req, res) {
-  res.render("index");
+  res.render("index", {
+    title: config.site.name
+  });
 };
