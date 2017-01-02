53c6ed76783560d025b271752b6af067fd96d24c
Anthony Michelizzi <cynical89@gmail.com>
2017-01-01 17:11:46 -0800

    Authorize secure route, login and sign up forms

--- controllers/secure.js
+++ controllers/secure.js
@@ -1,11 +1,18 @@
 const config = require("../config.json");
 
+let user;
+
 module.exports.index = function index(req, res) {
+  if(this.isAuthenticated()) {
   res.render("secure/index", {
     layout: "secure",
     title: config.site.name,
+    user: user,
     script: "notedrag"
   });
+  } else {
+    res.redirect("/login")
+  }
 };
 
 module.exports.submitNotes = function submitNote(req, res) {
