ce7ad0b7df808cff063fddd3ffca40169b91fb69
Anthony Michelizzi <cynical89@gmail.com>
2017-01-07 14:48:21 -0800

    Change from bcrypt to bcryptjs

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const db = require("../helpers/db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
 	newUser: (username, password) => {
